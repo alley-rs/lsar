9992bd12aa35dbbb4301f2424c61783be7685bfb
thepoy <thepoy@163.com>
2024-08-29 08:50:47 +0800

    优化空历史样式

--- src/components/history/index.tsx
+++ src/components/history/index.tsx
@@ -1,8 +1,10 @@
-import { For, Show, useContext } from "solid-js";
+import { For, lazy, Show, useContext } from "solid-js";
 import HistoryItem from "./item";
 import { Flex } from "alley-components";
 import "./index.scss";
 import { AppContext } from "~/context";
+
+const LazyEmpty = lazy(() => import("alley-components/lib/components/empty"));
 
 interface HistoryProps {
   items?: HistoryItem[];
@@ -12,8 +14,15 @@ const History = (props: HistoryProps) => {
   const [{ refetchHistoryItems }] = useContext(AppContext)!;
 
   return (
-    <Flex id="history" direction="vertical">
-      <Show when={props.items?.length} fallback={<span>空空如也</span>}>
+    <Flex
+      id="history"
+      class={props.items?.length ? undefined : "history-empty"}
+      direction="vertical"
+    >
+      <Show
+        when={props.items?.length}
+        fallback={<LazyEmpty description="空空如也" />}
+      >
         <For each={props.items}>
           {(item) => (
             <HistoryItem {...item} onDelete={() => refetchHistoryItems()} />
