98580ae8bf5b3476aaf260abac255ab8ede051ad
thepoy <thepoy@163.com>
2024-08-30 18:19:31 +0800

    chore: 使用条件编译消除 linux 中对未使用的 `window` 变量的警告

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -111,20 +111,23 @@ fn setup(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
     app.handle()
         .plugin(tauri_plugin_updater::Builder::new().build())?;
 
-    let window = app.get_webview_window("main").unwrap();
-
-    #[cfg(target_os = "macos")]
+    #[cfg(any(target_os = "macos", target_os = "windows"))]
     {
-        use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
-        apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
-            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
-    }
+        let window = app.get_webview_window("main").unwrap();
 
-    #[cfg(target_os = "windows")]
-    {
-        use window_vibrancy::apply_acrylic;
-        apply_acrylic(&window, Some((18, 18, 18, 125)))
-            .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+        #[cfg(target_os = "macos")]
+        {
+            use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
+            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+        }
+
+        #[cfg(target_os = "windows")]
+        {
+            use window_vibrancy::apply_acrylic;
+            apply_acrylic(&window, Some((18, 18, 18, 125)))
+                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+        }
     }
 
     #[cfg(desktop)]
